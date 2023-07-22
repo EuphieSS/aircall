300e78534f2dee9067a327e14a3dbc6ef0c3a9b1
--replace-all <samma118@hotmail.com>
2023-07-22 14:55:47 -0400

    Test API functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,21 @@
 import React from 'react';
+import { useState, useEffect } from 'react';
 import ReactDOM from 'react-dom';
+import Axios from 'axios';
 
 import Header from './Header.jsx';
 
 const App = () => {
+  // const [calls, setCalls] = useState({});
+
+  useEffect(() => {
+    Promise.all([
+      Axios.get("https://cerulean-marlin-wig.cyclic.app/activities") //because of the proxy in package.json, no need to include http://localhost:8001
+    ]).then((all) => {
+      console.log(all[0].data);
+    });
+  }, []);
+
   return (
     <div className='container'>
       <Header/>
