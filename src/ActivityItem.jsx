956ff87ba60c55b1cc682e0d0a180d83610c4cc2
--replace-all <samma118@hotmail.com>
2023-07-23 02:54:45 -0400

    Replace inbound/outbound texts with icons

--- src/ActivityItem.jsx
+++ src/ActivityItem.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import FeatherIcon from "feather-icons-react";
 
 const ActivityItem = (props) => {
   const { caller, callType, direction, time, archived } = props;
@@ -8,9 +9,19 @@ const ActivityItem = (props) => {
     return shortened;
   }
 
+  const callDirection = (call) => {
+    if (call === "inbound") {
+      return <FeatherIcon icon="phone-incoming" size="18" />
+    } else if (call === "outbound") {
+      return <FeatherIcon icon="phone-outgoing" size="18" />
+    } else {
+      return <FeatherIcon icon="phone" size="18" />
+    }
+  }
+
   return (
     <div className="call-info">
-      <div className="direction">{direction}</div>
+      <div className="direction">{callDirection(direction)}</div>
       <div className="detail">
         <div>{caller ? caller : "Unknown Caller"}</div>
         <div className="date-and-type">
